6ca24363ff78d89d15cad6642abc2b7a791700d1
Cyto-plasm <ruparakshit25@gmail.com>
2026-09-20 21:24:40 +0530

    Add session 2 String Operations class problems

---
+++ src/main/java/String Operations/class_problems/MaskedPhNumFormatter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MaskedPhNumFormatter {
+    public static String maskPhoneNumber(String phone) {
+
+        if (phone.length() != 10) {
+            return "Invalid phone number";
+        }
+
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+        }
+
+        String lastFour = phone.substring(6);
+
+        StringBuilder maskedNumber = new StringBuilder("XXXXXX");
+        maskedNumber.insert(6, "-");
+        maskedNumber.append(lastFour);
+
+        return maskedNumber.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String phone = sc.nextLine();
+
+        System.out.println(maskPhoneNumber(phone));
+    }
+}
